2c0c054631d3cab014c9fdb9f358e5369fe1ca7c
AdamStrzempa <adstrzempa@gmail.com>
2017-05-10 14:45:11 +0200

    Toggle Hidden Details on a React Native Event Card

--- index.android.js
+++ index.android.js
@@ -3,131 +3,92 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View,
-  PanResponder,
   Animated,
-  TextInput,
-  ScrollView
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity
 } from 'react-native';
 
+import Portland from "./portland.jpg";
 
-const getTransformStyle = (animation) => {
-  return {
-    transform: [
-      {translateY: animation}
-    ]
-  }
-}
 export default class AndroidButton extends Component {
   
+   constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+    this.toggleCard = this.toggleCard.bind(this);
+  }
   componentWillMount() {
     this.animated = new Animated.Value(0);
-    this.animatedMargin = new Animated.Value(0);
-    this.scrollOffset = 0;
-    this.contentHeight = 0;
-    this.scrollViewHeight = 0;
-    
-    this.panResponder = PanResponder.create({
-      onMoveShouldSetPanResponder: (evt, gestureState) => {
-        
-      },
-      onPanResponderGrant: (e, gestureState) => {
-        const { dy } = gestureState
-        const totalScrollHeight = this.scrollOffset + this.scrollViewHeight
-
-        if((this.scrollOffset < 0 && dy > 0) || ((totalScrollHeight > this.contentHeight) && dy < 0))
-        {
-          return true;
-        }
-      },
-      onPanResponderMove: (e, gestureState) => {
-        const { dy } = gestureState;
-        if (dy < 0) {
-          this.animated.setValue(dy);
-        }
-        else if (dy>0) {
-          this.animatedMargin.setValue(dy);
-        }
-    
-      },
-      onPanResponderRelease: (e, gestureState) => {
-        const {dy} = gestureState;
-
-        if(dy < -150) {
-          Animated.parallel([
-            Animated.timing(this.animated, {
-              toValue: -400,
-              duration: 150,
-            }),
-            Animated.timing(this.animatedMargin, {
-              toValue: 0,
-              duration: 150,
-            })
-          ]).start();
-        }
-        else if (dy > -150 && dy < 150) 
-        {
-          Animated.parallel([
-            Animated.timing(this.animated, {
-              toValue: 0,
-              duration: 150,
-            }),
-            Animated.timing(this.animatedMargin, {
-              toValue: 0,
-              duration: 150,
-            })
-          ]).start();
-        }
-        else if (dy > 150) {
-          Animated.timing(this.animated, {
-            toValue: 400,
-            duration: 300
-          }).start();
-        }
-      }
+  }
+  toggleCard() {
+    this.setState((state) => ({
+      open: !state.open
+    }), () => {
+      const toValue = this.state.open ? 1 : 0;
+      Animated.timing(this.animated, {
+        toValue,
+        duration: 500
+      }).start();
     })
-        
   }
   render() {
-    const spacerStyle = {
-      marginTop: this.animatedMargin
-    }
-    const opacityInterpolate = this.animated.interpolate({
-      inputRange: [-400, 0, 400],
-      outputRange: [0, 1, 0]
+    const offsetInterpolate = this.animated.interpolate({
+      inputRange: [0, 1],
+      outputRange: [191, 0]
     })
-    const modelStyle = {
+    const arrowRotate = this.animated.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["180deg", "0deg"]
+    })
+
+    const offsetStyle = {
       transform: [
-        {translateY: this.animated }
-      ],
-      opacity: opacityInterpolate
+        {
+          translateY: offsetInterpolate
+        }
+      ]
+    }
+
+    const arrowStyle = {
+      transform: [
+        {
+          rotate: arrowRotate
+        }
+      ]
+    }
+
+    const opacityStyle = {
+      opacity: this.animated
     }
     return (
-      <View style={styles.conteinter}>
-        <Animated.View style={spacerStyle} />
-        <Animated.View style={[styles.modal, modelStyle]} {...this.panResponder.panHandlers}>
-          <View style={styles.comments}>
-            <ScrollView
-              scrollEventThrottel={16}
-              onScroll={event => {
-                this.scrollOffset = event.nativeEvent.contentOffset.y;
-                this.scrollViewHeight = event.nativeEvent.layoutMeasurement.height
-              }}
-              onContentSizeChange={(contentWidth, contentHeight) => {
-                this.contentHeight = contentHeight;
-              }}
-            >
-              <Text style={styles.fakeText}>Top</Text>
-              <Text style={styles.fakeComments} />
-              <Text style={styles.fakeText}>Top</Text>
-            </ScrollView>
-          </View>
-          <View style={styles.inputWrap}>
-            <TextInput style={styles.textInput} placeholder="comment" />
-          </View>
-        </Animated.View>
-      </View>  
-    
+      <View style={styles.container}>
+        <Image source={Portland} resizeMode="cover" style={styles.background}>
+          <Animated.View style={[styles.card, offsetStyle]}>
+            <TouchableOpacity onPress={this.toggleCard}>
+              <View style={styles.header}>
+                <View>
+                  <Text style={styles.title}>Portland, Oregon</Text>
+                  <Text style={styles.date}>June 24th</Text>
+                </View>
+                <View style={styles.arrowContainer}>
+                  <Animated.Text style={[styles.arrow, arrowStyle]}>↓</Animated.Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+            <Animated.View style={[styles.scrollViewWrap, opacityStyle]}>
+              <ScrollView contentContainerStyle={styles.scrollView}>
+                <Text>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget sodales est. Donec facilisis, urna nec scelerisque pellentesque, nulla est euismod nunc, sed lacinia ex nunc placerat neque. Proin malesuada venenatis leo. Etiam viverra ipsum nec justo pharetra, eget rutrum enim eleifend. Ut eu mollis mi. Aenean eget nisl nibh. Sed sed elit eget nisi tincidunt elementum vitae vitae orci. Phasellus porta vitae purus eu molestie. Nulla cursus eros odio, sit amet pellentesque felis semper eu. Mauris id facilisis libero. Nullam posuere sed magna quis aliquam. Praesent sodales vulputate sollicitudin. Ut in rutrum eros, ac facilisis augue. Suspendisse consequat, erat ut convallis tincidunt, enim sem auctor ligula, sit amet congue arcu ligula at tortor. Morbi a elit varius, blandit tellus suscipit, tincidunt erat. Mauris feugiat cursus bibendum.
+                </Text>
+              </ScrollView>
+            </Animated.View>
+          </Animated.View>
+        </Image>
+      </View>
     );
   }
 }
@@ -135,32 +96,51 @@ export default class AndroidButton extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 30
+    alignItems: "center",
+    backgroundColor: "#152536"
   },
-  modal: {
-    flex: 1,
-    borderRadius: 15,
+  background: {
+    width: 300,
+    height: 250,
+    borderRadius: 3,
     overflow: "hidden",
     borderWidth: 1,
-    borderColor: "#333"
+    borderColor: "rgba(255,255,255,.5)"
   },
-  comments: {
-    flex: 1
-  },
-  fakeComments: {
-    height: 1000,
-    backgroundColor: "#f1f1f1"
-  },
-  inputWrap: {
-    flexDirection: "row",
-    paddingHorizontal: 15
-  },
-  textInput: {
+  scrollViewWrap: {
     flex: 1,
-    height: 50,
-    borderTopWidth: 1,
-    borderTopColor: "#000"
-}
+  },
+  card: {
+    backgroundColor: "#FFF",
+    flex: 1,
+    paddingHorizontal: 15,
+    paddingVertical: 4,
+    transform: [{
+      translateY: 191,
+    }]
+  },
+  scrollView: {
+    marginTop: 15
+  },
+  header: {
+    flexDirection: "row"
+  },
+  title: {
+    fontSize: 24,
+    color: "#333"
+  },
+  date: {
+    fontSize: 18,
+    color: "#333"
+  },
+  arrowContainer: {
+    flex: 1,
+    alignItems: "flex-end",
+    justifyContent: "center"
+  },
+  arrow: {
+    fontSize: 30,
+    color: "#333"
+  }
 });
-
 AppRegistry.registerComponent('AndroidButton', () => AndroidButton);
